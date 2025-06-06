664806195ebf22d3e0add92318b92b21789321d1
Viktoriia Popel <xructa2009@gmail.com>
2025-06-06 15:10:12 +0100

    fix 2 task

--- js/task-2.js
+++ js/task-2.js
@@ -1,7 +1,17 @@
-function getShippingMessage(country, price, deliveryFee) {
-  return `Shipping to ${country} will cost ${price + deliveryFee} credits`;
-}
+function formatMessage(message, maxLength ) {
+  if (message.length <= maxLength) {
+    return message;}
+      else {
+        return message.slice(0, maxLength)  + "...";}
+      }
+  
+  
+  
 
-console.log(getShippingMessage("Australia", 120, 50));
-console.log(getShippingMessage("Germany", 80, 20));
-console.log(getShippingMessage("Sweden", 100, 20));
+
+console.log(formatMessage("Curabitur ligula sapien", 16));
+console.log(formatMessage("Curabitur ligula sapien", 23));
+console.log(formatMessage("Vestibulum facilisis purus nec", 20));
+console.log(formatMessage("Vestibulum facilisis purus nec", 30));
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15));
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41));
