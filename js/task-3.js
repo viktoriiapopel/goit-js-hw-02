fb4aa1c52836994572b729478082ccd0b4c5e225
Viktoriia Popel <xructa2009@gmail.com>
2025-06-06 16:59:47 +0100

    fix 3 task

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,13 @@
-function getElementWidth(content, padding, border) {
-  const contentValue = parseFloat(content);
-  const paddingValue = parseFloat(padding);
-  const borderValue = parseFloat(border);
-
-  const totalWidth = contentValue + 2 * paddingValue + 2 * borderValue;
-  return totalWidth;
-  
+function checkForSpam(message) {
+const LowerCaseMessage = message.toLowerCase();
+return (LowerCaseMessage.includes('spam') || LowerCaseMessage.includes('sale'));
 }
 
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
